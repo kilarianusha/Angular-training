0e840c62712a9ab0a0eb2163050b36a55abd02e1
Anusha Kilari <kilarianusha@Anushas-Air.fios-router.home>
2021-08-08 00:50:19 -0400

    angular app

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'app-server' ,
-    templateUrl : '../server/server.component.html'
+    templateUrl : '../server/server.component.html',
 
 
 })
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html'
+  templateUrl: './servers.component.html',
 })
 export class ServersComponent implements OnInit {
 
